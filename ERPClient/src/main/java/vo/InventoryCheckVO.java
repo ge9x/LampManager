07373e7e04c1bed4a47877c5142dd9c5fd223f05
巽 <161250042@mail.smal.nju.edu.cn>
2017-10-21 16:18:33 +0800

    Write InventoryCheckVO class.

--- ERPClient/src/main/java/vo/InventoryCheckVO.java
+++ ERPClient/src/main/java/vo/InventoryCheckVO.java
@@ -1,5 +1,20 @@
 package vo;
 
-public class InventoryCheckVO {
+import java.util.Date;
+import java.util.HashMap;
 
+/**
+ * Created on 2017/10/21
+ * @author 巽
+ *
+ */
+public class InventoryCheckVO {
+	/**
+	 * 盘点日期
+	 */
+	Date date;
+	/**
+	 * 每个商品->库存均价
+	 */
+	HashMap<GoodsVO, Double> averagePrice;
 }
