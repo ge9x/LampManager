fa285a4aafa6f88353cc3e5874e009d77cf47fdd
巽 <161250042@mail.smal.nju.edu.cn>
2017-10-21 10:45:55 +0800

    Create ClassificationVO class.

--- ERPClient/src/main/java/vo/ClassificationVO.java
+++ ERPClient/src/main/java/vo/ClassificationVO.java
@@ -1,5 +1,41 @@
 package vo;
 
-public class ClassificationVO {
+import java.util.ArrayList;
 
+/**
+ * Created on 2017/10/21
+ * @author 巽
+ *
+ */
+public class ClassificationVO {
+	/**
+	 * 商品分类ID
+	 */
+	String id;
+	/**
+	 * 商品分类名称
+	 */
+	String name;
+	/**
+	 * 该商品分类的父分类
+	 */
+	ClassificationVO father;
+	/**
+	 * 该商品分类的所有子分类
+	 */
+	ArrayList<ClassificationVO> chidren;
+	/**
+	 * 该商品分类下的所有商品
+	 */
+	ArrayList<GoodsVO> goods;
+	
+	public ClassificationVO(String id, String name, ClassificationVO father, ArrayList<ClassificationVO> chidren,
+			ArrayList<GoodsVO> goods) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.father = father;
+		this.chidren = chidren;
+		this.goods = goods;
+	}
 }
