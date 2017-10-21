c31f220206f640f3a062156402bdc8a5786f6c8e
zlk <576970072@qq.com>
2017-10-21 09:46:24 +0800

    zlkV0

---
+++ ERPClient/src/main/java/blservice/customerblservice/CustomerBLService.java
@@ -0,0 +1,63 @@
+package blservice.customerblservice;
+
+import java.util.ArrayList;
+
+import vo.CustomerAddVO;
+import vo.CustomerVO;
+
+/**
+ * created by zlk on 2017/10/21
+ */
+
+public interface CustomerBLService {
+	  //管理客户界面得到客户编号
+	/**
+	 * 创建客户时得到客户编号
+	 * 
+	 * @return customerID
+	 * @author zlk
+	 * 
+	 */
+	public long getNewCustomerID();
+      //管理客户的步骤
+	/**
+	 * 管理客户中的添加客户
+	 * 
+	 * @param vo
+	 * @return 处理信息
+	 * @author zlk
+	 */
+	public ResultMessage addCustomer(CustomerAddVO vo);
+	/**
+	 * 管理客户中的删除客户
+	 * 
+	 * @param name
+	 * @return 处理信息
+	 * @author zlk
+	 */
+	public ResultMessage deleteCustomer(String name);
+	/**
+	 * 通过关键字查找客户
+	 * 
+	 * @param keywords
+	 * @return 满足条件的客户
+	 * @author zlk
+	 */
+	public ArrayList<CustomerVO> findCustomerByKeywords(String keywords);
+	/**
+	 * 通过客户编号查找客户
+	 * 
+	 * @param customerID
+	 * @return 满足条件的客户
+	 * @author zlk
+	 */
+	public ArrayList<CustomerVO> findCustomerByCustomerID(String customerID);
+	/**
+	 * 管理客户中的更新客户
+	 * 
+	 * @param vo
+	 * @return 处理信息
+	 * @author zlk
+	 */
+	public ResultMessage updateCustomer(CustomerAddVO vo);
+}
