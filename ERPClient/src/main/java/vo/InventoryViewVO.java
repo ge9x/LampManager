ea949a63d484eee5533f42437b5ff2c045af4a37
巽 <161250042@mail.smal.nju.edu.cn>
2017-10-22 10:39:18 +0800

    Make InventoryViewItemVO class a independent public class.

--- ERPClient/src/main/java/vo/InventoryViewVO.java
+++ ERPClient/src/main/java/vo/InventoryViewVO.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
-import util.InventoryViewItemType;
-
 /**
  * Created on 2017/10/21
  * @author 巽
@@ -41,38 +39,5 @@ public class InventoryViewVO {
 		this.inventory = inventory;
 		this.item = item;
 		this.total = total;
-	}
-}
-
-/**
- * 一张单据中一种商品的库存变动条目<br><br>
- * Created on 2017/10/21
- * @author 巽
- *
- */
-class InventoryViewItemVO{
-	/**
-	 * 商品
-	 */
-	public GoodsVO goods;
-	/**
-	 * 库存变动类型
-	 */
-	public InventoryViewItemType type;
-	/**
-	 * 数量
-	 */
-	public int amount;
-	/**
-	 * 金额
-	 */
-	public double price;
-	
-	public InventoryViewItemVO(GoodsVO goods, InventoryViewItemType type, int amount, double price) {
-		super();
-		this.goods = goods;
-		this.type = type;
-		this.amount = amount;
-		this.price = price;
 	}
 }
