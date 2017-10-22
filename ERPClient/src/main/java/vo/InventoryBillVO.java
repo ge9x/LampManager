16aff6fe9d6084cd7641b50cf76f867f77fc9426
巽 <161250042@mail.smal.nju.edu.cn>
2017-10-22 23:20:15 +0800

    Add inventory and user member variables into InventoryBillPO/VO.

--- ERPClient/src/main/java/vo/InventoryBillVO.java
+++ ERPClient/src/main/java/vo/InventoryBillVO.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import util.BillState;
 import util.BillType;
+import util.UserPosition;
 
 /**
  * Created on 2017/10/21
@@ -12,28 +13,20 @@ import util.BillType;
  *
  */
 public class InventoryBillVO extends BillVO{
-//	/**
-//	 * 单据ID
-//	 */
-//	String id;
-//	/**
-//	 * 单据类型
-//	 */
-//	BillType type;
-//	/**
-//	 * 单据状态
-//	 */
-//	BillState state;
-//	/**
-//	 * 单据提交时间
-//	 */
-//	Date date;
+	/**
+	 * 该单据涉及的仓库
+	 */
+	public String inventory;
+	/**
+	 * 操作员
+	 */
+	public UserPosition user;
 	/**
 	 * 该单据内的商品对应的溢出/缺损/赠送/报警数量（报警数量为商品警戒数量，实际数量由商品引用取得）
 	 */
 	public HashMap<GoodsVO, Integer> goodsMap;
 	
-	public InventoryBillVO(String ID, BillType type, BillState state, Date date, HashMap<GoodsVO, Integer> goodsMap) {
+	public InventoryBillVO(String ID, BillType type, BillState state, Date date, String inventory, UserPosition user, HashMap<GoodsVO, Integer> goodsMap) {
 		super();
 		this.ID = ID;
 		this.type = type;
