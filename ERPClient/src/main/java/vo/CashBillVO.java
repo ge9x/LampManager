c7d175d8369f6afaef5f1d2e8bb9a55497534c2a
161250053 <161250053@smail.nju.edu.cn>
2017-10-22 22:59:47 +0800

    为啥我的桩程序要写那么多假数据！！！

--- ERPClient/src/main/java/vo/CashBillVO.java
+++ ERPClient/src/main/java/vo/CashBillVO.java
@@ -1,6 +1,10 @@
 package vo;
 
+import util.BillState;
+import util.BillType;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Kry·L on 2017/10/22.
@@ -26,4 +30,15 @@ public class CashBillVO extends BillVO{
      * 总额
      */
     public double sum;
+
+    public CashBillVO(Date date, String ID, BillState state, BillType type, String userName, String accountName, ArrayList<CashBillItemVO> cashBillItems, double sum) {
+        this.date = date;
+        this.ID = ID;
+        this.state = state;
+        this.type = type;
+        this.userName = userName;
+        this.accountName = accountName;
+        this.cashBillItems = cashBillItems;
+        this.sum = sum;
+    }
 }
--- ERPServer/src/main/java/dataservice/financedataservice/FinanceDataService.java
+++ ERPServer/src/main/java/dataservice/financedataservice/FinanceDataService.java
@@ -1,10 +1,14 @@
 package dataservice.financedataservice;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
 import po.BillPO;
+import util.BillState;
+import util.BillType;
 import util.ResultMessage;
 
-import java.lang.reflect.Array;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 /**
  * Created by Kry·L on 2017/10/22.
@@ -14,21 +18,117 @@ public interface FinanceDataService {
     /**
      * 获得新的收款单ID
      * @return
+     * @throws RemoteException
      */
-    public String getNewReceiptID();
+    public String getNewReceiptID() throws RemoteException;
 
     /**
      * 获得新的付款单ID
      * @return
+     * @throws RemoteException
      */
-    public String getNewPaymentID();
+    public String getNewPaymentID() throws RemoteException;
 
     /**
      * 获得新的现金费用单
      * @return
+     * @throws RemoteException
      */
-    public String getNewCashBillID();
+    public String getNewCashBillID() throws RemoteException;
 
-    public ResultMessage addBill(BillPO po);
+    /**
+     * 添加新的单据
+     * @param po
+     * @return
+     * @throws RemoteException
+     */
+    public ResultMessage addBill(BillPO po) throws RemoteException;
+
+    /**
+     * 删除单据
+     * @param po
+     * @return
+     * @throws RemoteException
+     */
+    public ResultMessage deleteBill(BillPO po) throws RemoteException;
+
+    /**
+     * 更新单据
+     * @param po
+     * @return
+     * @throws RemoteException
+     */
+    public ResultMessage updateBill(BillPO po) throws RemoteException;
+
+    /**
+     * 根据单据类型查找
+     * @param typpe
+     * @return
+     * @throws RemoteException
+     */
+    public ArrayList<BillPO> findBillByType(BillType typpe) throws RemoteException;
+
+    /**
+     * 根据单据ID查找
+     * @param ID
+     * @return
+     * @throws RemoteException
+     */
+    public BillPO findBillByID(String ID) throws RemoteException;
+
+    /**
+     * 根据单据状态查找
+     * @param state
+     * @return
+     * @throws RemoteException
+     */
+    public ArrayList<BillPO> findBillByState(BillState state) throws RemoteException;
+
+    /**
+     * 显示所有单据
+     * @return
+     * @throws RemoteException
+     */
+    public ArrayList<BillPO> show() throws RemoteException;
+
+    /**
+     * 通过单据类型得到所有该类型单据ID
+     * @param type
+     * @return
+     * @throws RemoteException
+     */
+    public ArrayList<String> getAllIDByType(BillType type) throws RemoteException;
+
+    /**
+     * 获得该单据的起始时间和结束时间
+     * @param ID
+     * @return
+     * @throws RemoteException
+     */
+    public HashMap<Date,Date> getTimeRange(String ID) throws RemoteException;
+
+    /**
+     * 获得该单据的客户
+     * @param ID
+     * @return
+     * @throws RemoteException
+     */
+    public String getCustomer(String ID) throws RemoteException;
+
+    /**
+     * 获得该单据的业务员
+     * @param ID
+     * @return
+     * @throws RemoteException
+     */
+    public String getSalesman(String ID) throws RemoteException;
+
+    /**
+     * 获得该单据的仓库
+     * @param ID
+     * @return
+     * @throws RemoteException
+     */
+    public String getInventory(String ID) throws RemoteException;
 
 }
