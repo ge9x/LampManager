ac89fcd101c51beb93008e72c82ef85ce48a0a74
zlk <576970072@qq.com>
2017-11-05 16:35:28 +0800

    add

--- ERPClient/src/main/java/bl/salesbl/SalesController.java
+++ ERPClient/src/main/java/bl/salesbl/SalesController.java
@@ -23,85 +23,71 @@ public class SalesController implements SalesBLService{
 		sales=new Sales();
 	}
 	
-	@Override
 	public String getnewPurchaseID() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String getnewReturnID() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String getnewSalesID() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String getnewSalesReturnID() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ArrayList<PromotionBargainVO> showBargains() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ArrayList<PromotionCustomerVO> getFitPromotionCustomer() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ArrayList<PromotionTotalVO> getFitPromotionTotal() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public PurchaseVO addPurchase(PurchaseVO vo) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void addGoodsItem(GoodsItemVO item) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public SalesVO addSales(SalesVO vo) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ResultMessage submitPurchase(PurchaseVO pur) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ResultMessage submitSales(SalesVO sal) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ResultMessage saveSales(SalesVO bill) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ResultMessage savePurchase(PurchaseVO bill) {
 		// TODO Auto-generated method stub
 		return null;
