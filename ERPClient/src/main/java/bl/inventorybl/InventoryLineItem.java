3c5d1caa606a5464bb1e3268d15b89950c932f8d
巽 <161250042@mail.smal.nju.edu.cn>
2017-11-05 16:24:49 +0800

    Create classes as graphs promised.

---
+++ ERPClient/src/main/java/bl/inventorybl/InventoryLineItem.java
@@ -0,0 +1,12 @@
+package bl.inventorybl;
+
+import blservice.goodsblservice.GoodsInfo;
+
+/**
+ * Created on 2017/11/5
+ * @author 巽
+ *
+ */
+public class InventoryLineItem {
+	private GoodsInfo goodsInfo;
+}
