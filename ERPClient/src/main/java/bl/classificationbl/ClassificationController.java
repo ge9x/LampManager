80aa53c624d315aa3874e51381c4ff991332185d
巽 <161250042@mail.smal.nju.edu.cn>
2017-11-05 15:42:12 +0800

    Write fundamental variable and method.

--- ERPClient/src/main/java/bl/classificationbl/ClassificationController.java
+++ ERPClient/src/main/java/bl/classificationbl/ClassificationController.java
@@ -1,10 +1,51 @@
 package bl.classificationbl;
 
+import java.util.ArrayList;
+
+import blservice.classificationblservice.ClassificationBLService;
+import util.ResultMessage;
+import vo.ClassificationVO;
+
 /**
  * Created on 2017/11/5
  * @author 巽
  *
  */
-public class ClassificationController {
+public class ClassificationController implements ClassificationBLService{
+	
+	private Classification classification;
+
+	public ClassificationController(){
+		classification = new Classification();
+	}
+	
+	public ArrayList<ClassificationVO> show() {
+		return classification.show();
+	}
+
+	public ArrayList<ClassificationVO> find(String keyword) {
+		return classification.find(keyword);
+	}
+
+	public ClassificationVO showDetails(String ID) {
+		return classification.showDetails(ID);
+	}
+
+	public ResultMessage add(ClassificationVO vo) {
+		return classification.add(vo);
+	}
+
+	public ResultMessage delete(String ID) {
+		return classification.delete(ID);
+	}
+
+	public ResultMessage update(ClassificationVO vo) {
+		return classification.update(vo);
+	}
+
+	public String getNewID() {
+		return classification.getNewID();
+	}
+	
 
 }
