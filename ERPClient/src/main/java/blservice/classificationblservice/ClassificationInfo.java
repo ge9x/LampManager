e6cea7a1dd09c086ccbbb04b15bf9d3b7c12ab0a
巽 <161250042@mail.smal.nju.edu.cn>
2017-11-05 15:52:27 +0800

    Create two info interface.

---
+++ ERPClient/src/main/java/blservice/classificationblservice/ClassificationInfo.java
@@ -0,0 +1,10 @@
+package blservice.classificationblservice;
+
+/**
+ * Created on 2017/11/5
+ * @author 巽
+ *
+ */
+public interface ClassificationInfo {
+
+}
---
+++ ERPClient/src/main/java/blservice/goodsblservice/GoodsInfo.java
@@ -0,0 +1,10 @@
+package blservice.goodsblservice;
+
+/**
+ * Created on 2017/11/5
+ * @author 巽
+ *
+ */
+public interface GoodsInfo {
+
+}
