75e8e3777e0db20de1178e3b2f00d6072e1049a3
巽 <161250042@mail.smal.nju.edu.cn>
2017-11-07 22:23:50 +0800

    Write Java Doc.

--- ERPClient/src/main/java/bl/inventorybl/InventoryLineItem.java
+++ ERPClient/src/main/java/bl/inventorybl/InventoryLineItem.java
@@ -23,19 +23,43 @@ public class InventoryLineItem {
 		this.numberDifference = numberDifference;
 		this.totalPrice = totalPrice;
 	}
-	
+
+	/**
+	 * 得到商品的ID
+	 * @return 商品的ID
+	 */
 	public String getGoodsID() {
 		return goodsID;
 	}
+	
+	/**
+	 * 得到库存变动的类型
+	 * @return 库存变动的类型
+	 */
 	public InventoryListItemType getInventoryListItemType() {
 		return inventoryListItemType;
 	}
+	
+	/**
+	 * 得到商品的名称
+	 * @return 商品的名称
+	 */
 	public String getGoodsName() {
 		return goodsName;
 	}
+	
+	/**
+	 * 得到商品变动的数量（必为正数，通过库存变动类型指示增/减）
+	 * @return 商品变动的数量
+	 */
 	public int getNumberDifference() {
 		return numberDifference;
 	}
+	
+	/**
+	 * 得到变动的商品的总金额
+	 * @return 变动的商品的总金额
+	 */
 	public int getTotalPrice() {
 		return totalPrice;
 	}
