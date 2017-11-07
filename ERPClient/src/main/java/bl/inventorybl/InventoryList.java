2d469a349c331e6f36bc055de5bc19785612fa57
巽 <161250042@mail.smal.nju.edu.cn>
2017-11-07 22:28:26 +0800

    Write Java Doc.

--- ERPClient/src/main/java/bl/inventorybl/InventoryList.java
+++ ERPClient/src/main/java/bl/inventorybl/InventoryList.java
@@ -6,6 +6,7 @@ import blservice.salesblservice.SalesInfo;
 import util.ResultMessage;
 
 /**
+ * 库存变动情况的清单
  * Created on 2017/11/5
  * @author 巽
  *
@@ -17,14 +18,28 @@ public class InventoryList {
 		inventoryLineItem = new ArrayList<InventoryLineItem>();
 	}
 	
+	/**
+	 * 通过Sales单据添加一条库存变动情况
+	 * @param salesInfo Sales单据的信息
+	 * @return 是否成功添加
+	 */
 	public ResultMessage add(SalesInfo salesInfo){
 		return null;
 	}
 	
+	/**
+	 * 通过Sales单据删除一条库存变动情况
+	 * @param salesInfo Sales单据的信息
+	 * @return 是否成功删除
+	 */
 	public ResultMessage delete(SalesInfo salesInfo){
 		return null;
 	}
 	
+	/**
+	 * 得到所有库存变动情况
+	 * @return 含有所有库存变动情况的链表
+	 */
 	public ArrayList<InventoryLineItem> getList(){
 		return inventoryLineItem;
 	}
