20dccdebbe9a8835d87138cf21df948f74816e6e
巽 <161250042@mail.smal.nju.edu.cn>
2017-11-14 16:55:18 +0800

    Add two methods in GoodsInfo, and Create GoodsIdentityVO therefore.

--- ERPClient/src/main/java/blservice/goodsblservice/GoodsInfo.java
+++ ERPClient/src/main/java/blservice/goodsblservice/GoodsInfo.java
@@ -1,7 +1,9 @@
 package blservice.goodsblservice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import vo.GoodsIdentityVO;
 import vo.GoodsVO;
 
 /**
@@ -15,4 +17,15 @@ public interface GoodsInfo {
 	 * @return 包含所有商品的VO的链表
 	 */
 	public ArrayList<GoodsVO> getAllGoods();
+	/**
+	 * 得到所有商品的ID、名称和型号
+	 * @return 包含所有商品的ID、名称和型号的VO的链表
+	 */
+	public ArrayList<GoodsIdentityVO> getAllGoodsIdentity();
+	/**
+	 * 根据商品ID得到商品的进价和销售价
+	 * @param ID 商品ID
+	 * @return 包含商品进价和销售价的哈希表：HashMap<进价，销售价>
+	 */
+	public HashMap<Double, Double> getBuyingPriceAndRetailPriceByID(String ID);
 }
