263dbb4bc7dc8cc028fc73624f9fa7f9603851e2
161250053 <161250053@smail.nju.edu.cn>
2017-11-19 21:09:37 +0800

    银行账户的动态数据写好啦。。。一把辛酸泪

---
+++ ERPClient/src/main/java/ui/viewcontroller/FinancialStaff/FinancialAccountCellController.java
@@ -0,0 +1,51 @@
+package ui.viewcontroller.FinancialStaff;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import vo.AccountVO;
+
+/**
+ * Created by Kry·L on 2017/11/19.
+ */
+public class FinancialAccountCellController {
+
+    @FXML
+    Label BankName;
+
+    @FXML
+    Label DeleteIcon;
+
+    @FXML
+    Label EditIcon;
+
+    @FXML
+    Text Balance;
+
+    @FXML
+    public void initialize(){
+        DeleteIcon.setText("\ue619");
+        EditIcon.setText("\ue601");
+    }
+
+    public void setAccount(AccountVO account){
+//        BankName.setText(account.accountName);
+//        Balance.setText(getMoneyString(account.money));
+    }
+    private String getMoneyString(double money){
+        String raw = String.valueOf(money);
+        if (!raw.contains(".")){
+            raw = raw + ".00";
+        }
+        if ((raw.length() - raw.lastIndexOf(".")) != 3){
+            for (int i = 0; i < 3-(raw.length() - raw.lastIndexOf(".")); i++){
+                raw = raw + "0";
+            }
+        }
+        for (int i = raw.lastIndexOf(".")-3; i >= 0; i++){
+            raw = raw.substring(0,i) + "," + raw.substring(i);
+        }
+        return raw;
+    }
+
+}
--- ERPClient/src/main/java/ui/viewcontroller/FinancialStaff/FinancialAccountController.java
+++ ERPClient/src/main/java/ui/viewcontroller/FinancialStaff/FinancialAccountController.java
@@ -1,12 +1,18 @@
 package ui.viewcontroller.FinancialStaff;
 
 import blservice.accountblservice.AccountBLService;
+import blstubdriver.AccountBLService_Stub;
 import dataservice.accountdataservice.AccountDataService;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import vo.AccountVO;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -22,21 +28,46 @@ public class FinancialAccountController {
     @FXML
     TilePane accountList;
 
-    private static final int NUM_OF_CELLS = 6;
-    private AccountBLService accountBLService;
-    private ArrayList<AccountVO> accunts;
+    private static final int NUM_OF_CELLS = 4;
+    private FXMLLoader[] loaders = new FXMLLoader[NUM_OF_CELLS];
+    private VBox[] cells = new VBox[NUM_OF_CELLS];
+    AccountBLService accountBLService = new AccountBLService_Stub();
+    ArrayList<AccountVO> accounts;
+
 
     @FXML
     public void initialize(){
         addIcon.setText("\ue61e");
         searchIcon.setText("\ue69d");
 
-        accountList.setHgap(50);
+        accountList.setHgap(30);
         accountList.setVgap(20);
-        accountList.setPrefColumns(2);
+        accountList.setPrefColumns(3);
+
+
+        for (int i = 0; i < NUM_OF_CELLS; i++){
+            try {
+                FXMLLoader accountLoader = new FXMLLoader();
+                accountLoader.setLocation(getClass().getResource("/view/financialStaff/AccountCell.fxml"));
+                VBox cell = accountLoader.load();
+                loaders[i] = accountLoader;
+                cells[i] = cell;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        showAccountList();
     }
 
     public void showAccountList(){
+        accounts = accountBLService.show();
+
+        for (int i =0; i < accounts.size(); i++){
+            FinancialAccountCellController accountCellController = loaders[i].getController();
+            accountCellController.setAccount(accounts.get(i));
+            accountList.getChildren().add(cells[i]);
+        }
+
 
     }
 }
