f36730c5c3e0374cdb6e5c61e68ef0b3d5160c2c
zlk <576970072@qq.com>
2017-11-23 21:19:22 +0800

    add

--- ERPServer/src/main/java/po/InitAccountPO.java
+++ ERPServer/src/main/java/po/InitAccountPO.java
@@ -3,25 +3,40 @@ package po;
 import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  * Created by Kry·L on 2017/10/30.
  */
+
+@Entity
+@Table(name = "initAccount")
 public class InitAccountPO {
     private Date date;
-    private ArrayList<CustomerPO> customerPOS;
-    private ArrayList<AccountPO> accountPOS;
-    private ArrayList<GoodsPO> goodsPOS;
-    private ArrayList<ClassificationPO> classificationPOS;
+    private List<CustomerPO> customerPOS;
+    private List<AccountPO> accountPOS;
+    private List<GoodsPO> goodsPOS;
+    private List<ClassificationPO> classificationPOS;
 
-    public InitAccountPO(Date date, ArrayList<CustomerPO> customerPOS, ArrayList<AccountPO> accountPOS, ArrayList<GoodsPO> goodsPOS,ArrayList<ClassificationPO> classificationPOS) {
+    public InitAccountPO(Date date, List<CustomerPO> customerPOS, List<AccountPO> accountPOS, List<GoodsPO> goodsPOS,List<ClassificationPO> classificationPOS) {
         this.date = date;
         this.customerPOS = customerPOS;
         this.accountPOS = accountPOS;
         this.goodsPOS = goodsPOS;
         this.classificationPOS = classificationPOS;
     }
-
+    
+    @Id
+    @Temporal(TemporalType.TIMESTAMP)
     public Date getDate() {
         return date;
     }
@@ -30,35 +45,43 @@ public class InitAccountPO {
         this.date = date;
     }
 
-    public ArrayList<CustomerPO> getCustomerPOS() {
+    @OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name = "claid")
+    public List<CustomerPO> getCustomerPOS() {
         return customerPOS;
     }
 
-    public void setCustomerPOS(ArrayList<CustomerPO> customerPOS) {
+    public void setCustomerPOS(List<CustomerPO> customerPOS) {
         this.customerPOS = customerPOS;
     }
 
-    public ArrayList<AccountPO> getAccountPOS() {
+    @OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name = "claid")
+    public List<AccountPO> getAccountPOS() {
         return accountPOS;
     }
 
-    public void setAccountPOS(ArrayList<AccountPO> accountPOS) {
+    public void setAccountPOS(List<AccountPO> accountPOS) {
         this.accountPOS = accountPOS;
     }
 
-    public ArrayList<GoodsPO> getGoodsPOS() {
+    @OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name = "claid")
+    public List<GoodsPO> getGoodsPOS() {
         return goodsPOS;
     }
 
-    public void setGoodsPOS(ArrayList<GoodsPO> goodsPOS) {
+    public void setGoodsPOS(List<GoodsPO> goodsPOS) {
         this.goodsPOS = goodsPOS;
     }
 
-    public ArrayList<ClassificationPO> getClassificationPOS() {
+    @OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name = "claid")
+    public List<ClassificationPO> getClassificationPOS() {
         return classificationPOS;
     }
 
-    public void setClassificationPOS(ArrayList<ClassificationPO> classificationPOS) {
+    public void setClassificationPOS(List<ClassificationPO> classificationPOS) {
         this.classificationPOS = classificationPOS;
     }
 }
