4ac3b2456f6a113e5a379cd6cbf95d8f3fa1c6eb
Aster <161250213@mail.smal.nju.edu.com>
2017-11-23 20:50:26 +0800

    更新客户管理界面

--- ERPClient/src/main/java/ui/viewcontroller/Admin/AdminUserViewController.java
+++ ERPClient/src/main/java/ui/viewcontroller/Admin/AdminUserViewController.java
@@ -1,5 +1,36 @@
 package ui.viewcontroller.Admin;
 
+import java.util.ArrayList;
+
+import blservice.userblservice.UserBLService;
+import blstubdriver.UserBLService_Stub;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import vo.UserVO;
+
 public class AdminUserViewController {
 
+	@FXML
+    Label addIcon;
+
+    @FXML
+    Label searchIcon;
+
+    @FXML
+    TilePane UserList;
+    
+    private ArrayList<FXMLLoader> loaders = new ArrayList<FXMLLoader>();
+    private ArrayList<VBox> cells = new ArrayList<VBox>();
+    
+    UserBLService userBLService = new UserBLService_Stub();
+    AdminViewController adminViewController;
+    ArrayList<UserVO> users;
+    
+    @FXML
+    public void initialize(){
+    	
+    }
 }
--- ERPClient/src/main/java/ui/viewcontroller/SalesStaff/SalesStaffCustomerCellController.java
+++ ERPClient/src/main/java/ui/viewcontroller/SalesStaff/SalesStaffCustomerCellController.java
@@ -2,14 +2,19 @@ package ui.viewcontroller.SalesStaff;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+import util.Level;
+import vo.CustomerVO;
 
 public class SalesStaffCustomerCellController {
 
-	@FXML
-	Label CustomerIcon;
+	CustomerVO customer;
+	SalesStaffCustomerInfoViewController salesStaffCustomerInfoViewController;
 	
 	@FXML
-	Label LevelIcon;
+	Label CustomerIcon;
 	
 	@FXML
 	Label CustomerName;
@@ -21,7 +26,7 @@ public class SalesStaffCustomerCellController {
 	Label CustomerType;
 	
 	@FXML
-	Label Level;
+	Label CustomerLevel;
 	
 	@FXML
 	Label DetailIcon;
@@ -29,6 +34,45 @@ public class SalesStaffCustomerCellController {
     @FXML
     public void initialize(){
         CustomerIcon.setText("\ue671");
-        DetailIcon.setText("\ue601");
+        DetailIcon.setText("\ue89d");
+    }
+    
+    public void setSalesStaffCustomerInfoViewController(SalesStaffCustomerInfoViewController salesStaffCustomerInfoViewController){
+        this.salesStaffCustomerInfoViewController = salesStaffCustomerInfoViewController;
+    }
+    
+    public void setCustomer(CustomerVO customer){
+    	this.customer = customer;
+    	CustomerName.setText(customer.customerName);
+    	CustomerID.setText(customer.customerID);
+    	CustomerType.setText(customer.category.getValue());
+    	setLevel(customer.level);
+    }
+    
+    public void setLevel(Level level){
+    	if(level==Level.LEVEL_ONE){
+	    	CustomerLevel.setText("VIP1");
+	    	CustomerLevel.setStyle("-fx-background-color:#0066CC");
+    	}
+    	else if(level==Level.LEVEL_TWO){
+	    	CustomerLevel.setText("VIP2");
+	    	CustomerLevel.setStyle("-fx-background-color:#FFCCFF");
+    	}
+    	else if(level==Level.LEVEL_THREE){
+	    	CustomerLevel.setText("VIP3");
+	    	CustomerLevel.setStyle("-fx-background-color:#FFCC33");
+    	}
+    	else if(level==Level.LEVEL_FOUR){
+	    	CustomerLevel.setText("VIP4");
+	    	CustomerLevel.setStyle("-fx-background-color:#00FF99");
+    	}
+    	else if(level==Level.LEVEL_FIVE){
+	    	CustomerLevel.setText("VIP5");
+	    	CustomerLevel.setStyle("-fx-background-color:#CC99FF");
+    	}
+    }
+    
+    public void clickDetailButton(){
+    	
     }
 }
