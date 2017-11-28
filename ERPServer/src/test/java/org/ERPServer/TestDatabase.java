61a231ed9315346b7377149e64c457166e5a2711
zlk <576970072@qq.com>
2017-11-28 09:19:10 +0800

    modify the structure of impl

--- ERPServer/src/test/java/org/ERPServer/TestDatabase.java
+++ ERPServer/src/test/java/org/ERPServer/TestDatabase.java
@@ -9,11 +9,16 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import dataimpl.classificationdataimpl.ClassificationDataServiceImpl;
+import dataimpl.customerdataimpl.CustomerDataServiceImpl;
 //import dataimpl.goodsdataimpl.GoodsDataServiceImpl;
 import dataservice.classificationdataservice.ClassificationDataService;
+import dataservice.customerdataservice.CustomerDataService;
 import dataservice.goodsdataservice.GoodsDataService;
 import po.ClassificationPO;
+import po.CustomerPO;
 import po.GoodsPO;
+import util.CustomerCategory;
+import util.Level;
 
 public class TestDatabase {
 	SessionFactory sf;
@@ -74,12 +79,49 @@ public class TestDatabase {
 		
 		end(session);
 	}
+	
+	public void testCustomer(){
+		Session s = setUp();
+		CustomerDataService customerImpl=CustomerDataServiceImpl.getInstance();
+		
+		try{	
+			customerImpl.add(new CustomerPO(CustomerCategory.SELLER,Level.LEVEL_FIVE,"金主","15545786610",
+					"南京仙林大学城","421000","ddl@163.com",1.0,10000.0,0.0,"业务员1",125.0,400));
+			customerImpl.add(new CustomerPO(CustomerCategory.PUR_AGENT,Level.LEVEL_FIVE,"进货商1","15247678373",
+					"南京新街口","421001","dds@163.com",1.0,0.0,2000.0,"业务员2",224.0,500));
+		
+			ArrayList<CustomerPO> cus=customerImpl.show();
+			System.out.println("共有"+cus.size()+"记录：");
+			for(CustomerPO po:cus){
+				System.out.println(po.getCustomerID()+" "+po.getCustomerName()+" "+po.getAddress());
+			}
+			System.out.println("查询结束");
+			
+			ArrayList<CustomerPO> cus1=customerImpl.findByCustomerID(1);
+			System.out.println("共有"+cus1.size()+"记录：");
+			for(CustomerPO po:cus1){
+				System.out.println(po.getCustomerID()+" "+po.getCustomerName()+" "+po.getAddress());
+			}
+			System.out.println("查询结束");
+			
+			customerImpl.init();
+			ArrayList<CustomerPO> cus2=customerImpl.show();
+			System.out.println("共有"+cus2+"记录");
+			System.out.println("查询结束");
+			
+		}catch(RemoteException e){
+			System.out.println("Exception！");
+			e.printStackTrace();
+		}
+		end(s);
+	}
 
 	public static void main(String[] args) {
 		TestDatabase test = new TestDatabase();
 		
-		test.testClassification();
-		test.testGoods();
+		//test.testClassification();
+		//test.testGoods();
+		test.testCustomer();
 		
 		test.sf.close();
 	}
