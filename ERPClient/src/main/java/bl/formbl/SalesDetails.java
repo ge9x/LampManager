373c671972ae91df8b51fa2b9b96f4d62c7a1c13
161250053 <161250053@smail.nju.edu.cn>
2017-12-07 21:32:18 +0800

    报表

--- ERPClient/src/main/java/bl/formbl/SalesDetails.java
+++ ERPClient/src/main/java/bl/formbl/SalesDetails.java
@@ -4,7 +4,6 @@ import ExcelUtil.enums.ExcelType;
 import ExcelUtil.impl.ExportToExcel;
 import ExcelUtil.model.Model;
 import bean.SalesDetailsBean;
-import bl.goodsbl.Goods;
 import bl.salesbl.SalesController;
 import blservice.formblservice.SalesDetailsInput;
 import blservice.salesblservice.SalesInfo;
@@ -14,6 +13,7 @@ import vo.GoodsItemVO;
 import vo.SalesDetailVO;
 import vo.SalesVO;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -21,25 +21,80 @@ import java.util.ArrayList;
  */
 public class SalesDetails {
 
-    private ArrayList< SalesDetailsItem> salesDetailsItem;
-    ArrayList<String> salesIDS;
+    ArrayList<SalesVO> salesVOS;
     SalesInfo salesInfo = new SalesController();
 
     public SalesDetails(){
-        salesIDS = new ArrayList<>();
+        salesVOS = new ArrayList<>();
     }
     public ArrayList<SalesDetailVO> getSalesDetails(SalesDetailsInput input){
         ArrayList<SalesDetailVO> salesDetailVOS = new ArrayList<>();
 
-        salesIDS = salesInfo.getSalesOrderIDByDate(input.startDate,input.endDate);
-        if (input.filterType == FilterType.GOODS){
-
+        salesVOS = salesInfo.getAllSalesOrder(input.startDate,input.endDate);
+        if (input.filterType == FilterType.INVENTORY){
+            salesDetailVOS = searchByInventory(input.keyword);
+        }else if (input.filterType == FilterType.CUSTOMER){
+            salesDetailVOS = searchByCustomer(input.keyword);
+        }else if (input.filterType == FilterType.SALESMAN){
+            salesDetailVOS = searchBySalesman(input.keyword);
+        }else if (input.filterType == FilterType.GOODS){
+            salesDetailVOS = searchByGoodsName(input.keyword);
         }
-
         return salesDetailVOS;
-
-
     }
+
+    public ArrayList<SalesDetailVO> searchByInventory(String keyword){
+        ArrayList<SalesDetailVO> salesDetailVOS = new ArrayList<>();
+        for (SalesVO salesVO : salesVOS){
+            if (salesVO.inventory.contains(keyword)){
+                for (GoodsItemVO itemVO:salesVO.goodsItemList){
+                    SalesDetailVO vo = new SalesDetailVO(salesVO.date, itemVO.goodsName, itemVO.model, itemVO.number, itemVO.price);
+                    salesDetailVOS.add(vo);
+                }
+            }
+        }
+        return salesDetailVOS;
+    }
+
+    public ArrayList<SalesDetailVO> searchByCustomer(String keyword){
+        ArrayList<SalesDetailVO> salesDetailVOS = new ArrayList<>();
+        for (SalesVO salesVO : salesVOS){
+            if (salesVO.customer.contains(keyword)){
+                for (GoodsItemVO itemVO:salesVO.goodsItemList){
+                    SalesDetailVO vo = new SalesDetailVO(salesVO.date, itemVO.goodsName, itemVO.model, itemVO.number, itemVO.price);
+                    salesDetailVOS.add(vo);
+                }
+            }
+        }
+        return salesDetailVOS;
+    }
+
+    public ArrayList<SalesDetailVO> searchBySalesman(String keyword){
+        ArrayList<SalesDetailVO> salesDetailVOS = new ArrayList<>();
+        for (SalesVO salesVO : salesVOS){
+            if (salesVO.salesman.contains(keyword)){
+                for (GoodsItemVO itemVO:salesVO.goodsItemList){
+                    SalesDetailVO vo = new SalesDetailVO(salesVO.date, itemVO.goodsName, itemVO.model, itemVO.number, itemVO.price);
+                    salesDetailVOS.add(vo);
+                }
+            }
+        }
+        return salesDetailVOS;
+    }
+
+    public ArrayList<SalesDetailVO> searchByGoodsName(String keyword){
+        ArrayList<SalesDetailVO> salesDetailVOS = new ArrayList<>();
+        for (SalesVO salesVO : salesVOS){
+            for (GoodsItemVO itemVO:salesVO.goodsItemList) {
+                if (itemVO.goodsName.contains(keyword)){
+                    SalesDetailVO vo = new SalesDetailVO(salesVO.date, itemVO.goodsName, itemVO.model, itemVO.number, itemVO.price);
+                    salesDetailVOS.add(vo);
+                }
+            }
+        }
+        return salesDetailVOS;
+    }
+
     public ResultMessage export(String filePath, String filename, ArrayList<SalesDetailsBean> beans){
         ExportToExcel exporter = new ExportToExcel.Builder(filePath, filename, ExcelType.XLSX)
                 .withModel(Model.of(SalesDetailsBean.class, beans)).build();
