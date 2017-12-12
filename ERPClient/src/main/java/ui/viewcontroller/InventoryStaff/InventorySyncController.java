837d00d003fa47bfe5d4d81a5404aecc41a2cbc5
161250053 <161250053@smail.nju.edu.cn>
2017-12-12 21:10:44 +0800

    dev<ui>: inventory UI

--- ERPClient/src/main/java/ui/viewcontroller/InventoryStaff/InventorySyncController.java
+++ ERPClient/src/main/java/ui/viewcontroller/InventoryStaff/InventorySyncController.java
@@ -1,5 +1,7 @@
 package ui.viewcontroller.InventoryStaff;
 
+import bl.inventorybl.Inventory;
+import bl.inventorybl.InventoryController;
 import blservice.inventoryblservice.InventoryBLService;
 import blstubdriver.InventoryBLService_Stub;
 import com.jfoenix.controls.JFXTabPane;
@@ -14,6 +16,7 @@ import javafx.scene.control.Tab;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import ui.component.BillPane;
 import ui.viewcontroller.common.BillController;
 import util.BillType;
 import vo.AccountBillVO;
@@ -27,17 +30,17 @@ import java.util.ArrayList;
  * Created by Kry·L on 2017/11/27.
  */
 public class InventorySyncController {
-    InventoryBLService inventoryBLService = new InventoryBLService_Stub();
+    InventoryBLService inventoryBLService = new InventoryController();
     InventoryViewController inventoryViewController;
     InventorySyncEditController inventorySyncEditController;
 
-    TilePane billPane;
     ArrayList<VBox> billNodes = new ArrayList<>();
     ArrayList<FXMLLoader> fxmlLoaders = new ArrayList<>();
 
     ArrayList<InventoryBillVO> overflow;
     ArrayList<InventoryBillVO> loss;
     ArrayList<InventoryBillVO> gift;
+    BillPane billPane;
 
     @FXML
     Label addIcon;
@@ -46,54 +49,41 @@ public class InventorySyncController {
     JFXTabPane tabPane;
 
     @FXML
+    VBox vBox;
+
+    @FXML
     public void initialize(){
         addIcon.setText("\ue61e");
+
         overflow = inventoryBLService.findBillByType(BillType.OVERFLOW);
         loss = inventoryBLService.findBillByType(BillType.LOSS);
         gift = inventoryBLService.findBillByType(BillType.GIFT);
 
-        initTabPane();
+        billPane = new BillPane("报溢单","报损单","赠送单");
+        initTabs();
+        vBox.getChildren().add(billPane.getTabPane());
+        billPane.getTabPane().getSelectionModel().selectLast();
+        billPane.getTabPane().getSelectionModel().selectFirst();
     }
-    public void initTabPane(){
-        Tab overflowTab = new Tab();
-        Tab lossTab = new Tab();
-        Tab giftTab = new Tab();
-
-        overflowTab.setText("报溢单");
-        lossTab.setText("报损单");
-        giftTab.setText("赠送单");
-
-        initTabs(overflowTab,lossTab,giftTab);
-        tabPane.getTabs().addAll(overflowTab,lossTab,giftTab);
-        tabPane.getSelectionModel().selectFirst();
-    }
-    public void initTabs(Tab...tabs){
-        for (int i = 0; i < tabs.length; i++){
-            tabs[i].setOnSelectionChanged(new EventHandler<Event>() {
+    public void initTabs(){
+        ArrayList<Tab> tabs = billPane.getAllTabs();
+        for (int i = 0; i < tabs.size(); i++){
+            tabs.get(i).setOnSelectionChanged(new EventHandler<Event>() {
                 @Override
                 public void handle(Event event) {
                     Tab tab = (Tab)event.getSource();
                     if (tab.isSelected()){
-                        ScrollPane scrollPane = new ScrollPane();
-                        billPane = new TilePane();
-                        billPane.setPadding(new Insets(10,7,0,10));
-                        billPane.setPrefColumns(3);
-                        billPane.setHgap(25);
-                        billPane.setVgap(20);
-                        billPane.getChildren().clear();
                         billNodes.clear();
                         fxmlLoaders.clear();
                         loadBills(tab.getText());
-                        billPane.getChildren().addAll(billNodes);
-                        scrollPane.setContent(billPane);
-                        tab.setContent(scrollPane);
+                        billPane.setContent(tab,billNodes);
                     }
                 }
             });
         }
     }
     public void loadBills(String tab){
-        ArrayList<InventoryBillVO> bills = null;
+        ArrayList<InventoryBillVO> bills = new ArrayList<>();
         switch (tab){
             case "报溢单": bills = overflow;break;
             case "报损单": bills = loss;break;
@@ -106,10 +96,13 @@ public class InventorySyncController {
                 VBox node = loader.load();
                 fxmlLoaders.add(loader);
                 billNodes.add(node);
-                BillController billController = loader.getController();
-                billController.hideCheckbox();
-                billController.setInventorySyncController(this);
-                billController.setBill(bills.get(i));
+                BillController financialBillController = loader.getController();
+                financialBillController.hideCheckbox();
+                if (tab == "草稿单据"){
+                    financialBillController.showDeleteIcon();
+                }
+                financialBillController.setInventorySyncController(this);
+                financialBillController.setBill(bills.get(i));
             } catch (IOException e) {
                 e.printStackTrace();
             }
