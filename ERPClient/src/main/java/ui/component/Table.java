5950201070f199569cba4f88731a5edf46423dbf
161250053 <161250053@smail.nju.edu.cn>
2017-12-15 23:33:28 +0800

    fix<inventory>: check bug

--- ERPClient/src/main/java/ui/component/Table.java
+++ ERPClient/src/main/java/ui/component/Table.java
@@ -14,16 +14,30 @@ import java.util.Observable;
  * Created by Kry·L on 2017/12/15.
  */
 public class Table<T> {
+
     TableView tableView;
+
     public  ObservableList<T> data = FXCollections.observableArrayList();
 
     public Table(){
         tableView = new TableView();
         tableView.setItems(data);
     }
+
+    /**
+     * 设置可否编辑
+     * @param editable
+     */
     public void setEditable(boolean editable){
         tableView.setEditable(editable);
     }
+
+    /**
+     * 增加新的一列
+     * @param name 列名称
+     * @param target 列对应的数据对象T的成员变量
+     * @param width 烈的宽度
+     */
     public void addColumn(String name, String target, double width){
         TableColumn column = new TableColumn(name);
         column.setPrefWidth(width);
@@ -31,7 +45,24 @@ public class Table<T> {
 
         tableView.getColumns().add(column);
     }
+
+    /**
+     * 获得表格
+     * @return
+     */
     public TableView getTable(){
         return tableView;
+    }
+
+    /**
+     * 增加一行
+     * @param row
+     */
+    public void addRow(T row){
+        data.add(row);
+    }
+
+    public void clear() {
+        data.clear();
     }
 }
