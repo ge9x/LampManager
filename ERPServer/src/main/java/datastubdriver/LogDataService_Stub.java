bed32435b4426603cb8cf56aae3b157559f9b206
巽 <161250042@mail.smal.nju.edu.cn>
2017-12-25 18:15:09 +0800

    fix(data): add log details in LogDataService_Stub

--- ERPServer/src/main/java/datastubdriver/LogDataService_Stub.java
+++ ERPServer/src/main/java/datastubdriver/LogDataService_Stub.java
@@ -12,15 +12,17 @@ import util.ResultMessage;
 
 /**
  * Created on 2017/12/25
+ * 
  * @author 巽
  *
  */
-public class LogDataService_Stub implements LogDataService{
+public class LogDataService_Stub implements LogDataService {
 	private ArrayList<LogPO> logs;
-	
+
 	{
 		logs = new ArrayList<>();
-		logs.add(new LogPO(LocalDateTime.now(), "00001", OperationType.ADD, OperationObjectType.CLASSIFICATION, ""));
+		logs.add(new LogPO(LocalDateTime.now(), "00001", OperationType.ADD, OperationObjectType.GOODS,
+				"ID:0300001, 名称:圣洁牌经典黑白配落地灯, 型号:SJ-0001, 所属分类:落地灯, 警戒数量:250, 进价:233, 零售价:250, 最近进价:233.3, 最近零售价: 250.5, 总数量: 600"));
 	}
 
 	@Override
@@ -30,7 +32,7 @@ public class LogDataService_Stub implements LogDataService{
 
 	@Override
 	public ArrayList<LogPO> getLogByDate(LocalDateTime start, LocalDateTime end) throws RemoteException {
-		if(start.isEqual(logs.get(0).getDateAndTime())){
+		if (start.isEqual(logs.get(0).getDateAndTime())) {
 			return logs;
 		}
 		return new ArrayList<>();
@@ -38,7 +40,7 @@ public class LogDataService_Stub implements LogDataService{
 
 	@Override
 	public ResultMessage addLog(LogPO po) throws RemoteException {
-		if(po.getUserID().equals("00001")){
+		if (po.getUserID().equals("00001")) {
 			logs.add(po);
 			return ResultMessage.SUCCESS;
 		}
