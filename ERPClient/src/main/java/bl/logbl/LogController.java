91fa8dba79eb360312ad8a63558b634062670043
巽 <161250042@mail.smal.nju.edu.cn>
2017-12-25 19:42:27 +0800

    refactor(bl): temporarily save.

---
+++ ERPClient/src/main/java/bl/logbl/LogController.java
@@ -0,0 +1,11 @@
+package bl.logbl;
+
+/**
+ * Created on 2017/12/25
+ * 
+ * @author 巽
+ *
+ */
+public class LogController {
+
+}
---
+++ ERPClient/src/main/java/blservice/logblservice/LogBLService.java
@@ -0,0 +1,10 @@
+package blservice.logblservice;
+
+/**
+ * Created on 2-17/12/25
+ * @author 巽
+ *
+ */
+public interface LogBLService {
+
+}
