c46ce211ca47beb706ba7a2ef06afb7834cae184
Aster <161250213@mail.smal.nju.edu.com>
2017-12-28 00:45:41 +0800

    feature(ui):消息盒子界面实现（内容为桩）

--- ERPClient/src/main/java/ui/viewcontroller/common/StateBarController.java
+++ ERPClient/src/main/java/ui/viewcontroller/common/StateBarController.java
@@ -2,7 +2,11 @@ package ui.viewcontroller.common;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 import org.apache.poi.hssf.record.LabelSSTRecord;
 import org.apache.xmlbeans.impl.jam.annotation.LineDelimitedTagParser;
@@ -58,9 +62,8 @@ public class StateBarController {
     ScrollPane scrollPane2;
     
     IntegerProperty numOfInfo = new SimpleIntegerProperty(0);
-    
-    private ArrayList<FXMLLoader> loaders = new ArrayList<>();
-    private ArrayList<Pane> cells = new ArrayList<>();
+    ArrayList<Integer> hasRemove = new ArrayList<>();
+    static int count = 0;
 
     @FXML
     public void initialize(){
@@ -85,89 +88,99 @@ public class StateBarController {
 		});
 
         ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setPrefSize(650.0, 400.0);
+        scrollPane.setPrefSize(480.0, 300.0);
         scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 
 
         vBox = new VBox();
-        vBox.setPrefWidth(649.0);
+        vBox.setPrefWidth(479.0);
 
         scrollPane.setContent(vBox);
 
         JFXPopup popup = new JFXPopup(scrollPane);
-        badge.setOnMouseClicked(e -> popup.show(InfoIcon, PopupVPosition.TOP, PopupHPosition.LEFT, -580, 35));
+        badge.setOnMouseClicked(e -> popup.show(InfoIcon, PopupVPosition.TOP, PopupHPosition.LEFT, -400, 35));
         
-        addMessageCell(BillState.PASS, LocalDate.now().toString(), "XSD-20171227-00001");
-        addMessageCell(BillState.FAILED, LocalDate.now().toString(), "XSD-20171227-00001");
-        addMessageCell(BillState.SUBMITTED, LocalDate.now().toString(), "XSD-20171227-00001");
-        addMessageCell(BillState.FAILED, LocalDate.now().toString(), "XSD-20171227-00001");
-        addMessageCell(BillState.PASS, LocalDate.now().toString(), "XSD-20171227-00001");
-        addMessageCell(BillState.SUBMITTED, LocalDate.now().toString(), "XSD-20171227-00001");
+        addMessageCell(BillState.PASS, LocalDate.now().toString() + " " +LocalTime.now().toString(), "XSD-20171227-00001");
+        addMessageCell(BillState.FAILED, LocalDate.now().toString() + " " +LocalTime.now().toString(), "XSD-20171227-00001");
+        addMessageCell(BillState.SUBMITTED, LocalDate.now().toString() + " " +LocalTime.now().toString(), "XSD-20171227-00001");
+        addMessageCell(BillState.FAILED, LocalDate.now().toString() + " " +LocalTime.now().toString(), "XSD-20171227-00001");
+        addMessageCell(BillState.PASS, LocalDate.now().toString() + " " +LocalTime.now().toString(), "XSD-20171227-00001");
+        addMessageCell(BillState.SUBMITTED, LocalDate.now().toString() + " " +LocalTime.now().toString(), "XSD-20171227-00001");
         
     }
     
     public void addMessageCell(BillState state, String time, String billID){
-    	AnchorPane messageCellPane = new AnchorPane();
-    	messageCellPane.setPrefSize(160.0, 70.0);
-    	Label Icon = new Label();
-    	Icon.setPrefSize(35, 35);
-    	Icon.setLayoutX(10);
-    	Icon.setLayoutY(8);
-    	Label hint = new Label();
-    	hint.setPrefSize(75, 35);
-    	hint.setLayoutX(57);
-    	hint.setLayoutY(8);
-    	hint.setWrapText(true);
-    	Label date = new Label();
-    	date.setPrefSize(97, 15);
-    	date.setLayoutX(57);
-    	date.setLayoutY(50);
-    	hint.setFont(new Font("Microsoft YaHei", 12));
-    	date.setFont(new Font("Microsoft YaHei", 10));
-
-    	Icon.setAlignment(Pos.CENTER);
-    	Icon.setStyle("-fx-font-family: iconfont;-fx-font-size: 30px;-fx-text-fill: #707070;");
-
-    	date.setText(time);
-		if(state == BillState.SUBMITTED){
-			Icon.setText("\ue602");
-			hint.setText("您有一张单据已提交");
-		}
-		else if(state == BillState.PASS){
-			Icon.setText("\ue625");
-			hint.setText("您有一张单据已通过");
-		}
-		else if(state == BillState.FAILED){
-			Icon.setText("\ue624");
-			hint.setText("您有一张单据未通过");
-		}
-    	
-    	messageCellPane.getChildren().add(Icon);
-    	messageCellPane.getChildren().add(hint);
-    	messageCellPane.getChildren().add(date);
-    	messageCellPane.setOnMouseClicked(new EventHandler<Event>() {
-
-			@Override
-			public void handle(Event arg0) {
-//				// TODO Auto-generated method stub
-//				Label label = null;
-//				if(state == BillState.SUBMITTED){
-//					label = new Label("已提交");
-//				}
-//				else if(state == BillState.PASS){
-//					label = new Label("已通过");
-//				}
-//				else if(state == BillState.FAILED){
-//					label = new Label("未通过");
-//				}
-//				scrollPane2.setContent(label);
-                hint.requestFocus();
-			}
-
-		});
+    	AnchorPane messageCellPane = null;
+//    	messageCellPane.setPrefSize(649.0, 70.0);
+//    	Label Icon = new Label();
+//    	Icon.setPrefSize(35, 35);
+//    	Icon.setLayoutX(10);
+//    	Icon.setLayoutY(8);
+//    	Label hint = new Label();
+//    	hint.setPrefSize(600, 35);
+//    	hint.setLayoutX(57);
+//    	hint.setLayoutY(8);
+//    	hint.setWrapText(true);
+//    	Label date = new Label();
+//    	date.setPrefSize(97, 15);
+//    	date.setLayoutX(650);
+//    	date.setLayoutY(50);
+//    	hint.setFont(new Font("Microsoft YaHei", 12));
+//    	date.setFont(new Font("Microsoft YaHei", 10));
+//
+//    	Icon.setAlignment(Pos.CENTER);
+//    	Icon.setStyle("-fx-font-family: iconfont;-fx-font-size: 30px;-fx-text-fill: #707070;");
+//
+//    	date.setText(time);
+//		if(state == BillState.SUBMITTED){
+//			Icon.setText("\ue602");
+//			hint.setText("您有一张单据已提交");
+//		}
+//		else if(state == BillState.PASS){
+//			Icon.setText("\ue625");
+//			hint.setText("您有一张单据已通过");
+//		}
+//		else if(state == BillState.FAILED){
+//			Icon.setText("\ue624");
+//			hint.setText("您有一张单据未通过");
+//		}
+//    	
+//    	messageCellPane.getChildren().add(Icon);
+//    	messageCellPane.getChildren().add(hint);
+//    	messageCellPane.getChildren().add(date);
+//    	messageCellPane.setOnMouseClicked(new EventHandler<Event>() {
+//
+//			@Override
+//			public void handle(Event arg0) {
+//                hint.requestFocus();
+//			}
+//
+//		});
+    	try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/common/MessageCell.fxml"));
+            messageCellPane = loader.load();
+            MessageCellController messageCellController = loader.getController();
+            messageCellController.setMessage(state, time, billID);
+            messageCellController.setStateBarController(this);
+            messageCellController.setOrder(count);
+            
+        }catch (IOException e){
+            e.printStackTrace();
+        }
         
         vBox.getChildren().add(messageCellPane);
         numOfInfo.set(numOfInfo.get()+1);
+        count++;
+    }
+    
+    public void deleteMessageCell(int order){
+    	hasRemove.add(order);
+    	Collections.sort(hasRemove);
+    	int index = hasRemove.indexOf(order);
+    	vBox.getChildren().remove(order-index);
+    	numOfInfo.set(numOfInfo.get()-1);
     }
 
     public void showMessage(String message){
