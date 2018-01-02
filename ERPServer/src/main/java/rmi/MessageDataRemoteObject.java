86575a99d842d6cd70363b98351f46a19a7b3a4e
巽 <161250042@mail.smal.nju.edu.cn>
2018-01-02 17:18:49 +0800

    feat(bl): add message data module

--- ERPServer/src/main/java/rmi/MessageDataRemoteObject.java
+++ ERPServer/src/main/java/rmi/MessageDataRemoteObject.java
@@ -4,35 +4,34 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
+import dataimpl.messagedataimpl.MessageDataServiceImpl;
 import dataservice.messagedataservice.MessageDataService;
 import po.MessagePO;
 import util.ResultMessage;
 import util.UserPosition;
 
 public class MessageDataRemoteObject extends UnicastRemoteObject implements MessageDataService{
-	
+	private static final long serialVersionUID = 5499526200765834945L;
 	private MessageDataService messageDataService;
 	
 	protected MessageDataRemoteObject() throws RemoteException{
-		
+		super();
+		messageDataService = MessageDataServiceImpl.getInstance();
 	}
 
 	@Override
-	public ArrayList<MessagePO> show(UserPosition position) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<MessagePO> show(UserPosition position) throws RemoteException {
+		return messageDataService.show(position);
 	}
 
 	@Override
-	public ResultMessage add(MessagePO po) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResultMessage add(MessagePO po) throws RemoteException {
+		return messageDataService.add(po);
 	}
 
 	@Override
-	public ResultMessage delete(int messageID) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResultMessage delete(int messageID) throws RemoteException {
+		return messageDataService.delete(messageID);
 	}
 
 }
